e38f7e1d4017551d5034c713756b171d62a45646
qorbaxk <qorbaxk97@gmail.com>
2023-02-23 23:11:07 +0900

    [style] 전체 틀, 가격 및 배송 스타일 추가

--- src/App.js
+++ src/App.js
@@ -3,20 +3,27 @@ import './style.css';
 
 export default function App() {
   return (
-    <div>
-      <img src="img/ProductImage.svg" alt="제품 이미지" />
-      <h1>래피젠 코로나 자가 검사 키트 2개입X1박스</h1>
-      <div>
-        <p>88%</p>
-        <p>56,760원</p>
-        <p>6,370원</p>
+    <div className="container">
+      <img
+        src="img/ProductImage.svg"
+        alt="제품 이미지"
+        className="product-img"
+      />
+      <h1 className="product-title">
+        <span>래피젠 코로나 자가 검사 키트</span>
+        <span>2개입X1박스</span>
+      </h1>
+      <div className="product-price">
+        <p className="product-price--discount-rate">88%</p>
+        <p className="product-price--before-price">56,760원</p>
+        <p className="product-price--after-price">6,370원</p>
       </div>
-      <div>
-        <p>택배배송</p>
-        <p>
+      <div className="product-delivery">
+        <span className="product-delivery--way">택배배송</span>
+        <span className="product-delivery--price">
           3,000원 <b>(주문시 결제)</b>
-        </p>
-        <p>10개마다 부과</p>
+        </span>
+        <p className="product-delivery--rule">10개마다 부과</p>
       </div>
       <div>
         <button>-</button>
