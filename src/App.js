737ed9f4a98509a95576164f3bfe8f1984229255
qorbaxk <qorbaxk97@gmail.com>
2023-02-23 10:10:29 +0900

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style.css";
+import {productImg} from "./assets/product-img.svg";
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <img src={productImg} alt="제품 이미지"/>
+      
     </div>
   );
 }
